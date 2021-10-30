7fa6c43e93d14b5e6636414b82fece8c56ed37d0
Obaidul Nasir <83164409+obaidulnasir@users.noreply.github.com>
2021-10-30 12:04:53 +0600

    all events show in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Home.css"
 
 const Home = () => {
+    const [allEvents, setAllEvents]= useState([]);
+    useEffect(()=>{
+        fetch("http://localhost:5000/allEvents")
+        .then(res => res.json())
+        .then(data => setAllEvents(data));
+    },[])
+    
     return (
         <div>
            <div className="banner bg-success">
@@ -18,6 +25,17 @@ const Home = () => {
                        <p className="w-50 mx-auto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam nulla dolores, placeat rerum dignissimos accusantium. Reiciendis delectus ratione iste odio! Incidunt error fuga, minima cupiditate numquam officiis natus quasi adipisci!</p>
                    </div>
                </div>
+           </div>
+           <div className="container">
+            <div className="row">
+            {
+                allEvents.slice(1, 10).map(events=><div key= {events._id} className="col-md-4">
+                    <h5>{events.title}</h5>
+                    <p>{events.description}</p>
+                    <button className="btn btn-warning">Button</button>
+                </div>)
+            }
+            </div>
            </div>
         </div>
     );
